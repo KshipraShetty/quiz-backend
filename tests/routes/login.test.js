883e8cbd61afb31b1b37b560a0164fda33f3f0f6
kshiprashetty <kshiprashetty@gmail.com>
2018-03-07 11:35:53 +0530

    added new test

--- tests/routes/login.test.js
+++ tests/routes/login.test.js
@@ -1,20 +1,25 @@
 const server = require('../../src/server');
+const models = require('../../models');
 
 describe('Check for login api', () => {
-  test('Check for statusCode of a valid username', (done) => {
+  beforeAll((done) => {
+    models.user.create({
+      username: 'hello',
+    }).then(() => done());
+  });
+  test('Check for statusCode of a new user', (done) => {
     const options = {
       method: 'POST',
       url: '/login',
       payload: {
-        username: 'hello',
+        username: 'newUser',
       },
     };
-    server.inject(options)
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
+    server.inject(options, (response) => {
+      expect(response.statusCode).toBe(201);
+      done();
+    });
+  }, 15000);
   test('Check for a statusCode of invalid user entry', (done) => {
     const options = {
       method: 'POST',
@@ -23,10 +28,30 @@ describe('Check for login api', () => {
         username: 0,
       },
     };
-    server.inject(options)
-      .then((response) => {
-        expect(response.statusCode).toBe(400);
-        done();
-      });
+    server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(400);
+      done();
+    });
+  });
+
+  test('Check for a statusCode of invalid user entry', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/login',
+      payload: {
+        username: 'a',
+      },
+    };
+    server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(400);
+      done();
+    });
+  });
+  afterAll((done) => {
+    models.user.destroy({
+      where: {
+        username: 'hello',
+      },
+    }).then(() => done());
   });
 });
