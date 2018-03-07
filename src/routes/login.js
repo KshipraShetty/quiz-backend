07bd34c6d003607f63d13e2c36d739dc961ee97f
kshiprashetty <kshiprashetty@gmail.com>
2018-03-07 11:36:27 +0530

    added login functionality

---
+++ src/routes/login.js
@@ -0,0 +1,123 @@
+const axios = require('axios');
+const models = require('../../models');
+const joi = require('joi');
+
+const createUsers = username =>
+  models.user.findOrCreate({
+    where: {
+      username,
+    },
+    defaults: {
+      username,
+    },
+  });
+// .then((response) => {
+//   if (response[1]) {
+//     return Promise.resolve('not exists');
+//   }
+//   return Promise.resolve('exists');
+// });
+
+
+const fetchQuestionDatabase = () => models.question.findAll()
+  .then(questions => questions);
+
+const checkQuestions = () => models.question.findAll()
+  .then((allQuestions) => {
+    if (allQuestions.length === 0) {
+      return populateQuestionDatabase();
+    } return fetchQuestionDatabase();
+  });
+
+const populateQuestionDatabase = () => {
+  const allQuestions = [];
+  const axiosGet =
+  axios.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allQuestions')
+    .then((questionsFromDB) => {
+      questionsFromDB.data.allQuestions.forEach((eachQuestion) => {
+        const newQuest = {};
+        newQuest.option = {};
+        for (const key in eachQuestion) {
+          if (key.indexOf('option') > -1) {
+            newQuest.option[key] = eachQuestion[key];
+          }
+        }
+        newQuest.questionId = eachQuestion.questionId;
+        newQuest.question = eachQuestion.question;
+        allQuestions.push(newQuest);
+      });
+      return models.question.bulkCreate(allQuestions);
+    });
+  return axiosGet;
+};
+
+const populateAnswers = () => {
+  let answerDB;
+  const allPromise = [];
+  return models.question.findAll()
+    .then(allQuestions => allQuestions.map((eachQuestion) => {
+      axios.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/${eachQuestion.questionId}`)
+        .then((answers) => {
+          answerDB = models.answer.create({
+            questionId: eachQuestion.questionId,
+            answer: answers.data.answer,
+
+          });
+          allPromise.push(answerDB);
+        });
+      return Promise.all(allPromise);
+    }));
+};
+
+
+const fetchAnswerDatabase = () => models.answer.findAll()
+  .then(answers => answers);
+
+const createAnswers = () =>
+  models.answer.findAll()
+    .then((allAnswers) => {
+      if (allAnswers.length === 0) {
+        return populateAnswers();
+      }
+      return fetchAnswerDatabase();
+    });
+
+
+const getAnswerOfUser = username => models.user.findAll({
+  where: {
+    username,
+  },
+  include: [{
+    model: models.useranswer,
+  }],
+});
+
+module.exports = [
+  {
+    method: 'POST',
+    path: '/login',
+    config: {
+      validate: {
+        payload: {
+          username: joi.string().min(3).required(),
+        },
+      },
+      handler: (req, resp) => createUsers(req.payload.username)
+        .then(userStatus => checkQuestions()
+          .then(questionStatus => createAnswers()
+            .then(() =>
+              getAnswerOfUser(req.payload.username)
+                .then((userAnswers) => {
+                  let statusCode = 200;
+                  if (userStatus[1]) {
+                    statusCode = 201;
+                  }
+                  resp({
+                    userAnswers,
+                    questionStatus,
+                    statusCode,
+                  });
+                })))),
+    },
+  },
+];
